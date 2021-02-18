6a694fdbca7885666bab19cc14e72486410e4e1e
zoicvil <891038713645Qaz>
2021-02-18 18:44:03 +0300

    Сокобан. Тестовое задание. [#465507]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,20 +4,18 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (VerticalWin(board, i) | HorizontalWin(board, i)) {
-                rsl = true;
-                break;
-            }
-
-
+            if ( board[i][i] == 1 & VerticalWin(board,i) || HorizontalWin(board, i)){
+                    rsl = true;
+                    break;
+                }
         }
         return rsl;
     }
 
     public static boolean VerticalWin(int[][] board, int vertical) {
         boolean rsl = true;
-        for (int[] ints : board) {
-            if (ints[vertical] == 0) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][vertical] == 0) {
                 rsl = false;
                 break;
             }

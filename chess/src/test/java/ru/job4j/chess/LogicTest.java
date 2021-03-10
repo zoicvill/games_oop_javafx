0dded8653e677abc7cc9fb0baf59403b85392382
zoicvil <891038713645Qaz>
2021-03-10 15:45:38 +0300

    Каркас шахматной доски [#465658]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -16,42 +16,13 @@ public class LogicTest {
 
     }
 
-    @Test
-    public void whenImpossibleMoveExpTestTry()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
-        try {
-            Logic logic = new Logic();
-            logic.add(new BishopBlack(Cell.C1));
-            logic.move(Cell.C1, Cell.H5);
-        } catch (ImpossibleMoveException e) {
-            Assert.assertEquals(String.format(
-                    "Could not way by diagonal from %s to %s", Cell.C1, Cell.H5),
-                    e.getMessage());
-        }
-    }
-
     @Test(expected = OccupiedCellException.class)
     public void whenOccupiedCellExp()
-            throws FigureNotFoundException, OccupiedCellException {
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException{
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.add(new BishopBlack(Cell.E3));
         logic.move(Cell.C1, Cell.H6);
-    }
-
-    @Test
-    public void whenOccupiedCellExpTestTry()
-            throws FigureNotFoundException, OccupiedCellException {
-        try {
-            Logic logic = new Logic();
-            logic.add(new BishopBlack(Cell.C1));
-            logic.add(new BishopBlack(Cell.E3));
-            logic.move(Cell.C1, Cell.H6);
-        } catch (OccupiedCellException o) {
-            Assert.assertEquals("Ячейка занята",
-                    o.getMessage());
-        }
-
     }
 
     @Test(expected = FigureNotFoundException.class)
@@ -60,20 +31,6 @@ public class LogicTest {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.D2, Cell.H6);
-    }
-
-    @Test
-    public void whenFigureNotFoundExpTestTry()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
-        try {
-            Logic logic = new Logic();
-            logic.add(new BishopBlack(Cell.C1));
-            logic.move(Cell.D2, Cell.H6);
-        }
-        catch (FigureNotFoundException f){
-            Assert.assertEquals("Фигуры нет на клетке.",f.getMessage());
-        }
-
     }
 
 }
